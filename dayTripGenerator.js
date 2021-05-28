6c9290fa36a132e024eabca10baeb8405e0490a6
unknown <jmschenkelberg@gmail.com>
2021-05-28 15:03:00 -0500

    if/else statement added

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,9 +1,13 @@
 "use strict."
 
+let transportation = ["a car", "a bus", "a train", "a plane"];
 let destination = ["Chicago", "Milwaukee","Madison","Green Bay"];
 let restaurant = ["italian food", "chinese food", "burgers", "chicken"];
-let transportation = ["car", "bus", "train", "plane"];
 let entertainment = ["a sporting event", "a concert", "a shopping mall", "the museum"];
+let chosenTransportation = (getRandomItem (transportation));
+let chosenDestination = (getRandomItem (destination));
+let chosenRestaurant = (getRandomItem (restaurant));
+let chosenEntertainment = (getRandomItem (entertainment));
 
 function getRandomItem(arr) {
     let randomIndex = Math.floor(Math.random() * arr.length);
@@ -13,22 +17,47 @@ function getRandomItem(arr) {
 }
 // console.log (getRandomItem (destination))
 
-let userData1 = prompt ("Are you ready to take a day trip?\nIf so, Type 'Yes'"); {
+let userData1 = prompt ("Welcome to the Day Trip Generator!\nIf you ready for your 1st suggestion, Type 'Yes'"); {
     if (userData1 === "Yes"){ 
-        console.log ("Take a " +(getRandomItem (transportation)) + " to "+ (getRandomItem (destination)) + " and eat " + (getRandomItem(restaurant)) + " at " + (getRandomItem (entertainment)) + ".");
+
+        console.log ("Take " + chosenTransportation + " to "+ chosenDestination + " and eat " + chosenRestaurant + " at " + chosenEntertainment + ".");
     }
-    else (userData1 !=="Yes");{
-        console.log ("Please refresh the browser if you want another suggestion.")
+    else {
+        console.log ("Refresh the browser when you want to proceed.")
     }
 }
-// let newSelectionDestination = prompt ("Do you want a different city?\nPlease type Yes")
-//     if (newSelectionDestination = "Yes");
-//         console.log ("Here's a new city -" + (getRandomItem (destination))); 
-let userDataConfirm = prompt ("Are you satisfied with this suggestion?\nIf so, please type 'confirm'");{
+let userDataConfirm = prompt ("Are you satisfied with this suggestion?\nIf so, please type 'confirm'\nIf you would like a completely new suggestion, type 'start over'\nIf you want new mode of transportation, type 'transport'\nIf you would like a new city, type 'city'\nIf you would like a new type of food, type 'food'\If you would like a new type of entertainment, type 'entertainment'");{
     if(userDataConfirm === "confirm") {
         console.log ("Trip Confirmed! Have a great day trip.");
     }
-    else {
-        console.log ("Here's a new suggestion!. Take a " +(getRandomItem (transportation)) + " to "+ (getRandomItem (destination)) + " and eat " + (getRandomItem(restaurant)) + " at " + (getRandomItem (entertainment)) + ".");
+    else if (userDataConfirm === "start over") {
+        console.log ("Here's a new suggestion!.Take " + chosenTransportation + " to "+ chosenDestination + " and eat " + chosenRestaurant + " at " + chosenEntertainment + ".");
+            }
+    else if (userDataConfirm === "transport") {
+        console.log ("How about " + chosenTransportation +"?");
     }
+    else if (userDataConfirm === "city") {
+        console.log ("How about " + chosenDestination +"?");
+    }
+    else if (userDataConfirm === "food") {
+        console.log ("How about " + chosenRestaurant +"?");
+    }
+    else if (userDataConfirm === "entertainment") {
+        console.log ("How about " + chosenTransportation +"?");
+    }    
+    else{
+        console.log ("That is not a valid option, please start over by refreshing your browser.")
+    } 
 }
+
+let newSelectionDestination = prompt ("Do you want a different city?\nPlease type Yes")
+    if (newSelectionDestination = "Yes");
+        console.log ("Here's a new city -" + (getRandomItem (destination))); 
+
+
+//console log the whole trip
+
+//print an initial trip
+//ask user if they like it
+//if not, rechoose
+//if yes, print the final trip
