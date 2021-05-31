86d1f309b2cfbbaeb1cdcf51f4b61b7d98183e9a
unknown <jmschenkelberg@gmail.com>
2021-05-30 21:47:48 -0500

    removed unnecessary code

--- dayTripGenerator.js
+++ dayTripGenerator.js
@@ -1,9 +1,9 @@
 "use strict."
 
-let transportation = ["a car", "a bus", "a train", "a plane", "a hoverboard"];
-let destination = ["Chicago", "Milwaukee","Madison","Green Bay","Sheboygan"];
-let restaurant = ["Olive Garden", "Panda Express", "McDonald's", "Popeye's"];
-let entertainment = ["a sporting event", "a concert", "a shopping mall", "the museum"];
+let transportation = ["a car", "a bus", "a train", "a plane", "a hoverboard", "an Uber", "a Lyft"];
+let destination = ["Chicago","Milwaukee","Madison","Green Bay","Sheboygan","West Allis","Grafton"];
+let restaurant = ["Olive Garden","Panda Express", "McDonald's", "Popeye's","Burger King","Buffalo Wild Wings"];
+let entertainment = ["a sporting event", "a concert", "a shopping mall", "a museum","a park"];
 let chosenTransportation = getRandomItem (transportation);
 let chosenDestination = getRandomItem (destination);
 let chosenRestaurant = getRandomItem (restaurant);
@@ -15,7 +15,7 @@ function getRandomItem(arr) {
 
     return item;
 }
-// console.log (getRandomItem (destination))
+
 
 let userData1 = prompt ("Welcome to the Day Trip Generator!\nIf you ready for your 1st suggestion, Type 'Yes'"); {
     if (userData1 === "Yes"){
@@ -60,20 +60,8 @@ switch (userDataConfirm){
             }        
         }
         break;
-    case "confirm new": {
-        console.log ("Trip Confirmed! Have a great day trip. "+ "You will be taking " + chosenTransportation + " to "+ chosenDestination + " to eat " + chosenRestaurant + " at " + chosenEntertainment + ".");
-            }
-        break;
     default: 
         console.log (userDataConfirm = prompt ("Are you satisfied with this suggestion?\nIf so, please type 'confirm'\nIf you would like a completely new suggestion, type 'new suggestion'"));
         break;
     }   
 }
-
-
-//console log the whole trip
-
-//print an initial trip
-//ask user if they like it
-//if not, rechoose
-//if yes, print the final trip
